18c7a3bb62f2a31829838f9698d55b2319757d72
moffa89 <85792173+moffa89@users.noreply.github.com>
2025-02-18 08:06:09 +0100

    Delete Tweaks/Win32PrioritySeparation_lowerLatency.bat

--- Tweaks/Win32PrioritySeparation_lowerLatency.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@echo Win32PrioritySeparation Script by moffa89
-@echo =============================================
-
-echo change "Win32PrioritySeparation" to 0x28(40)
-reg add HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl /t REG_DWORD /v Win32PrioritySeparation /d 28 /f
-
-pause
