6d410466a0beecb02d8fc6f68a06f2295414d0d4
moffa89 <85792173+moffa89@users.noreply.github.com>
2025-02-18 08:08:27 +0100

    Delete Tweaks/Disable Bufferlist Tracking.bat

--- Tweaks/Disable Bufferlist Tracking.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@echo Disable Bufferlist Tracking Script by moffa89
-@echo =============================================
-
-echo change "Bufferlist Tracking" to Disable (0)
-reg add HKLM\SYSTEM\CurrentControlSet\Services\NDIS\Parameters /t REG_DWORD /v TrackNblOwner /d 0 /f
-
-pause
