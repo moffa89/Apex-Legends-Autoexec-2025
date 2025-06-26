70fe2082f27cd3bd16874dba7133fc8cab858529
moffa89 <85792173+moffa89@users.noreply.github.com>
2025-06-26 19:43:10 +0200

    Update Network Performance Tuning.bat

--- Tweaks/Network Performance Tuning/Network Performance Tuning.bat
+++ Tweaks/Network Performance Tuning/Network Performance Tuning.bat
@@ -1,6 +1,5 @@
 @echo off
 net session >nul 2>&1 || (powershell start -verb runas '"%~0"' &exit /b)
 
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v SackOpts /t reg_dword /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\AFD\Parameters" /v FastSendDatagramThreshold /t reg_dword /d 64000 /f
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\NDIS\Parameters" /v RssBaseCpu /t reg_dword /d 00000001 /f
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v SackOpts /t reg_dword /d 00000000 /f
